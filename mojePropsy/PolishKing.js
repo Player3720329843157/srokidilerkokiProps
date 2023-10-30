0b14b27924a2901685d47474c52387830b6cbd22
Marcin <m@gmail.com>
2023-10-30 09:29:21 +0100

    Add first project in react native

---
+++ mojePropsy/PolishKing.js
@@ -0,0 +1,11 @@
+import {Text, View} from 'react-native';
+
+const PolishKing = ()=>{
+
+    return(
+        <View>
+            <Text>PolishKing is coming</Text>
+        </View>
+    )
+}
+export default PolishKing;
